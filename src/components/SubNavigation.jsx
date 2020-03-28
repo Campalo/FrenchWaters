d8acdb5132a280cf0827fa7cde6356b3cb67c542
Campalo <25295489+Campalo@users.noreply.github.com>
2020-03-28 21:21:28 +0100

    Create Subnavigation component

---
+++ src/components/SubNavigation.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navigation.css';
+
+function SubNavigation() {
+    return (
+        <div className="subNav">
+            <span>SubNavigation</span>
+        </div>
+    )
+}
+
+export default SubNavigation
