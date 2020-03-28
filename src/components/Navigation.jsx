cfa5639b24e4610df32b7aefd2e21f173b091b83
Campalo <25295489+Campalo@users.noreply.github.com>
2020-03-28 21:21:17 +0100

    Create Navigation component

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navigation.css';
+
+function Navigation() {
+    return (
+        <nav>
+            <span><b>Navigation</b></span>
+        </nav>
+    )
+}
+
+export default Navigation;
