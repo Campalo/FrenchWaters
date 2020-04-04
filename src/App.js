c186cd19ea769d1ecf263fcada7b811c53534551
Campalo <25295489+Campalo@users.noreply.github.com>
2020-04-04 20:00:51 +0200

    Fetch departments from geo.api.gouve.fr

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ const options = {
   threshold: 1 //100% target has crossed the intersection
 }
 
+//SideNav effect
 const observer = new IntersectionObserver(([intersection]) => {
   const enterTop = intersection.isIntersecting && intersection.intersectionRect.top <= 80 + intersection.target.clientHeight;
   const leaveTop = !intersection.isIntersecting && intersection.intersectionRect.top <= 80;
@@ -34,32 +35,57 @@ function App() {
    observer.observe(subNavigation)
   });
 
-  const departements = new Array (95)
-    .fill("")
-    .map( (_, index) => (index + 1).toString() )
-    .concat(["971", "972", "973", "974", "976"])
+  // const departements = new Array (95)
+  //   .fill("")
+  //   .map( (_, index) => (index + 1).toString() )
+  //   .concat(["971", "972", "973", "974", "976"])
 
-  departements.splice(19, 1, "2A", "2B")
+  // departements.splice(19, 1, "2A", "2B")
 
-  console.log(departements)
-
-  const [stations, setStations] = useState([]);
-  let hasFetched = false;
+  // const [stations, setStations] = useState([]);
+  // const [deptName, setDeptName] = useState([]);
 
   // TODO : stop infinite loop
-  async function fetchCountStations() {
-    const urls = departements.map( dept => `https://hubeau.eaufrance.fr/api/v1/qualite_nappes/stations?format=json&num_departement=${dept}`);
-    const requests = urls.map( url => fetch(url) )
-    const responses = await Promise.all(requests);
-    const responsesJson = responses.map( response => response.json())
-    const result = await Promise.all(responsesJson)
-    setStations(result)
-    console.log(stations)
+  // async function fetchCountStations() {
+  //   const urls = departements.map( deptTest => `https://hubeau.eaufrance.fr/api/v1/qualite_nappes/stations?format=json&num_departement=${dept}`);
+  //   const requests = urls.map( url => fetch(url) )
+  //   const responses = await Promise.all(requests);
+  //   const responsesJson = responses.map( response => response.json())
+  //   const result = await Promise.all(responsesJson)
+  //   setStations(result)
+  //   setDeptName(result)
+    // console.log('stations', stations)
+    // console.log('deptName', deptName)
+    // if (deptName.length > 0){
+    //   console.log('deptName-one', deptName[10].data[0].nom_departement)
+    // }
+  // }
+
+  // useEffect(() => {
+  //   fetchCountStations();
+  // });
+
+  // TEST
+  const [deptsTest, setDeptTest] = useState([]);
+  const [isloading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  async function fetchDepartements() {
+    console.log('test')
+    try {
+      const urlDept = 'https://geo.api.gouv.fr/departements/';
+      const response = await fetch(urlDept);
+      const responseJson = await response.json();
+      setDeptTest(responseJson);
+    } catch(err){
+      setError(err.message)
+    }
+    setLoading(false)
   }
 
-  useEffect(() => {
-    fetchCountStations();
-  });
+  useEffect( () => {
+    fetchDepartements();
+  }, []);
 
   return (
     <div className="App">
@@ -69,11 +95,29 @@ function App() {
       <main>
         <Content />
         <SubNavigation />
-        <p>Departements</p>
+        {/* <p>Departements</p>
         <ul>
-          {stations.map( (station, i) => (<li>{departements[i]}: {station.count} stations</li>))}
+          {stations.map( (station, i) => <li>{departements[i]}: {station.count} stations</li>)}
         </ul>
-        <br></br>
+        <br></br> */}
+
+        <p>Departements</p>
+
+        {error ? <p>{error}</p> : null}
+
+        {!isloading ? (
+          deptsTest.map( deptTest => {
+            const { code, nom } = deptTest ;
+            return (
+              <ul key={code}>
+                <li>{code} - {nom}</li>
+              </ul>
+            )
+          })
+        ) : (
+          <span>Loading...</span>
+        )}
+
       </main>
     </div>
   );
