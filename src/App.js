f806a6c588ec654caa36622de52610491719edb8
Campalo <25295489+Campalo@users.noreply.github.com>
2020-04-05 14:54:01 +0200

    Fix infinite loop in UseEffect executing fetchCountStations

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const options = {
   threshold: 1 //100% target has crossed the intersection
 }
 
-//SideNav effect : TODO : Fix it
+//SideNav effect
 const observer = new IntersectionObserver(([intersection]) => {
   const enterTop = intersection.isIntersecting && intersection.intersectionRect.top <= 80 + intersection.target.clientHeight;
   const leaveTop = !intersection.isIntersecting && intersection.intersectionRect.top <= 80;
@@ -37,57 +37,49 @@ function App() {
    observer.observe(subNavigation)
   });
 
-  // const departements = new Array (95)
-  //   .fill("")
-  //   .map( (_, index) => (index + 1).toString() )
-  //   .concat(["971", "972", "973", "974", "976"])
-
-  // departements.splice(19, 1, "2A", "2B")
-
-  // const [stations, setStations] = useState([]);
-  // const [deptName, setDeptName] = useState([]);
-
-  // TODO : stop infinite loop
-  // async function fetchCountStations() {
-  //   const urls = departements.map( deptTest => `https://hubeau.eaufrance.fr/api/v1/qualite_nappes/stations?format=json&num_departement=${dept}`);
-  //   const requests = urls.map( url => fetch(url) )
-  //   const responses = await Promise.all(requests);
-  //   const responsesJson = responses.map( response => response.json())
-  //   const result = await Promise.all(responsesJson)
-  //   setStations(result)
-  //   setDeptName(result)
-    // console.log('stations', stations)
-    // console.log('deptName', deptName)
-    // if (deptName.length > 0){
-    //   console.log('deptName-one', deptName[10].data[0].nom_departement)
-    // }
-  // }
-
-  // useEffect(() => {
-  //   fetchCountStations();
-  // });
-
-  // TEST
-  const [deptsTest, setDeptTest] = useState([]);
+  // Get departements number and name
+  const [depts, setDept] = useState([]);
   const [isloading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  async function fetchDepartements() {
-    console.log('test')
-    try {
-      const urlDept = 'https://geo.api.gouv.fr/departements/';
-      const response = await fetch(urlDept);
-      const responseJson = await response.json();
-      setDeptTest(responseJson);
-    } catch(err){
-      setError(err.message)
-    }
-    setLoading(false)
-  }
-
   useEffect( () => {
+    async function fetchDepartements() {
+      console.log('test')
+      try {
+        const urlDept = 'https://geo.api.gouv.fr/departements/';
+        const response = await fetch(urlDept);
+        const responseJson = await response.json();
+        setDept(responseJson);
+      } catch(err){
+        setError(err.message)
+      }
+      setLoading(false)
+    }
     fetchDepartements();
   }, []);
+
+
+  // Get Stations number by departement
+  const [stations, setStations] = useState([]);
+
+  useEffect( () => {
+    if (depts.length > 100) { // Wait for the 101 departements to be loaded before fetching the stations
+      async function fetchCountStations() {
+        console.log('depts.code', depts.map(dept => dept.code))
+        try {
+          const urls = depts.map( dept => `https://hubeau.eaufrance.fr/api/v1/qualite_nappes/stations?format=json&num_departement=${dept.code}`);
+          const requests = urls.map( url => fetch(url) )
+          const responses = await Promise.all(requests);
+          const responsesJson = responses.map( response => response.json())
+          const result = await Promise.all(responsesJson)
+          setStations(result)
+        } catch(err){
+          console.log(err.message)
+        }
+      }
+      fetchCountStations();
+    }
+  }, [depts]);
 
   return (
     <div className="App">
@@ -97,16 +89,16 @@ function App() {
       <main>
         <Content />
         <SubNavigation />
-        {/* <p>Departements</p>
+
+        <p>Stations</p>
         <ul>
-          {stations.map( (station, i) => <li>{departements[i]}: {station.count} stations</li>)}
+          {stations.map( (station, i) => <li key={depts[i].code}>{depts[i].code}: {station.count} stations</li>)}
         </ul>
-        <br></br> */}
 
         <h2>Departements</h2>
-        {error ? <p>{error}</p> : null}
+        {error ? <p><i>{error}</i></p> : null}
         <div className="list">
-          <DepartementList isloading={isloading} depts={deptsTest}/>
+          <DepartementList isloading={isloading} departements={depts}/>
         </div>
 
       </main>
@@ -117,19 +109,7 @@ function App() {
 export default App;
 
 
-// function DepartementList({isloading, depts}) {
-//   if (isloading) {
-//     return <span>Loading...</span>
-//   }
-
-//   return (
-//     <ul>
-//       {depts.map(dept => <li key={dept.code}>{dept.code} - {dept.nom}</li>)}
-//     </ul>
-//   )
-// }
-
-function DepartementList({isloading, depts}) {
+function DepartementList({isloading, departements}) {
   if (isloading) {
     return <span>Loading...</span>
   }
@@ -137,8 +117,8 @@ function DepartementList({isloading, depts}) {
   return (
     <List
       itemLayout="horizontal"
-      dataSource={depts}
-      renderItem={dept => (
+      dataSource={departements}
+      renderItem={dept => ( // Equal to: departements.map( dept => <li>{dept.code}-{dept.nom}</li>)
         <List.Item>
           <List.Item.Meta
             avatar={<Avatar src="https://images.unsplash.com/photo-1541103335697-086d3519c039?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=200&q=80" />}
