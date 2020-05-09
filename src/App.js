a702e406b21a1d8f7769b83317537c70f73a7130
Campalo <25295489+Campalo@users.noreply.github.com>
2020-05-09 16:44:58 +0200

    Extract StationList into a component

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Navigation from './components/Navigation';
 import SubNavigation from './components/SubNavigation';
 import Content from './components/Content';
 import DepartementList from './components/DepartementList';
+import StationsListForOneDept from './components/StationList';
 
-import { List, Avatar, Button } from 'antd';
 import { Spin } from 'antd';
 import 'antd/dist/antd.css';
 
@@ -186,25 +186,3 @@ export default App;
 
 
 
-function StationsListForOneDept({stations, showMeasurements}) {
-  return (
-    <List
-      itemLayout="horizontal"
-      dataSource={stations}
-      renderItem={station => ( station.nb_mesures_piezo > 0 ?
-        <List.Item>
-          <List.Item.Meta
-            avatar={<Avatar src="https://images.unsplash.com/photo-1533201357341-8d79b10dd0f0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=100&q=80" />}
-            title={`Piézomètre de ${station.nom_commune}`}
-            description={station.date_fin_mesure !== null ? `Date du dernier relevé : ${station.date_fin_mesure}` : 'Date non communiquée'}
-          />
-           <div className="btn-list">
-            <Button onClick={() => showMeasurements(station.code_bss, station.nom_commune)}>Select</Button>
-          </div>
-        </List.Item>
-        :
-        <div className='hidden'></div> // Do not show the station if it does not have any measurements
-      )}
-    />
-  )
-}
