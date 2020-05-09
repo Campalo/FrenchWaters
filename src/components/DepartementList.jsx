ef148bcbb22c21f4777b20dd45c7f2dcfd60941d
Campalo <25295489+Campalo@users.noreply.github.com>
2020-05-09 17:14:31 +0200

    Add infiniteScroll to Dept and Station lists

--- src/components/DepartementList.jsx
+++ src/components/DepartementList.jsx
@@ -1,25 +1,28 @@
 import React from "react";
 import { List, Avatar, Button } from 'antd';
+import InfiniteScroll from 'react-infinite-scroller';
 
 
 function DepartementList({departements, showStations}) {
     return (
-      <List
-        itemLayout="horizontal"
-        dataSource={departements}
-        renderItem={dept => ( // Equal to: departements.map( dept => <li>{dept.code}-{dept.nom}</li>)
-          <List.Item>
-            <List.Item.Meta
-              avatar={<Avatar src="https://images.unsplash.com/photo-1541103335697-086d3519c039?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=100&q=80" />}
-              title={`${dept.code} - ${dept.nom}`}
-              description={`${dept.count} stations`}
+        <InfiniteScroll>
+            <List
+                itemLayout="horizontal"
+                dataSource={departements}
+                renderItem={dept => ( // Equal to: departements.map( dept => <li>{dept.code}-{dept.nom}</li>)
+                <List.Item>
+                    <List.Item.Meta
+                    avatar={<Avatar src="https://images.unsplash.com/photo-1541103335697-086d3519c039?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=100&q=80" />}
+                    title={`${dept.code} - ${dept.nom}`}
+                    description={`${dept.count} stations`}
+                    />
+                    <div className="btn-list">
+                    <Button onClick={showStations} value={dept.code}>Select</Button>
+                    </div>
+                </List.Item>
+                )}
             />
-            <div className="btn-list">
-              <Button onClick={showStations} value={dept.code}>Select</Button>
-            </div>
-          </List.Item>
-        )}
-      />
+        </InfiniteScroll>
     )
   };
 
--- src/components/StationList.jsx
+++ src/components/StationList.jsx
@@ -1,27 +1,31 @@
 
 import React from "react";
 import { List, Avatar, Button } from 'antd';
+import InfiniteScroll from 'react-infinite-scroller';
+
 
 function StationsListForOneDept({stations, showMeasurements}) {
     return (
-        <List
-        itemLayout="horizontal"
-        dataSource={stations}
-        renderItem={station => ( station.nb_mesures_piezo > 0 ?
-            <List.Item>
-            <List.Item.Meta
-                avatar={<Avatar src="https://images.unsplash.com/photo-1533201357341-8d79b10dd0f0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=100&q=80" />}
-                title={`Piézomètre de ${station.nom_commune}`}
-                description={station.date_fin_mesure !== null ? `Date du dernier relevé : ${station.date_fin_mesure}` : 'Date non communiquée'}
-            />
-                <div className="btn-list">
-                <Button onClick={() => showMeasurements(station.code_bss, station.nom_commune)}>Select</Button>
-            </div>
-            </List.Item>
-            :
-            <div className='hidden'></div> // Do not show the station if it does not have any measurements
-        )}
-        />
+        <InfiniteScroll>
+            <List
+            itemLayout="horizontal"
+            dataSource={stations}
+            renderItem={station => ( station.nb_mesures_piezo > 0 ?
+                <List.Item>
+                <List.Item.Meta
+                    avatar={<Avatar src="https://images.unsplash.com/photo-1533201357341-8d79b10dd0f0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=100&q=80" />}
+                    title={`Piézomètre de ${station.nom_commune}`}
+                    description={station.date_fin_mesure !== null ? `Date du dernier relevé : ${station.date_fin_mesure}` : 'Date non communiquée'}
+                    />
+                    <div className="btn-list">
+                    <Button onClick={() => showMeasurements(station.code_bss, station.nom_commune)}>Select</Button>
+                </div>
+                </List.Item>
+                :
+                <div className='hidden'></div> // Do not show the station if it does not have any measurements
+                )}
+                />
+            </InfiniteScroll>
     )
 }
 
