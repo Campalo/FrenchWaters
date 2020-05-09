55bddcb5d0f1ae757e00e24da91ad4f1c4ec3c56
Campalo <25295489+Campalo@users.noreply.github.com>
2020-05-09 21:31:22 +0200

    Update navigation text

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -3,9 +3,6 @@ import React, { Fragment } from 'react';
 function Content() {
     return (
         <Fragment>
-            <h1>
-                Qualité des nappes d'eau souterraines en France
-            </h1>
             <p>
                 Une nappe d'eau souterraine est une eau contenue dans les interstices ou les fissures d'une roche du sous-sol.
                 <i>Selon la définition donnée par Hub'eau.</i>
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,10 +3,10 @@ import './Navigation.css';
 
 function Navigation() {
     return (
-        <nav id='nav'>
-            <span>Navigation</span>
-        </nav>
-    )
+      <nav id="nav">
+        <span>Les nappes d'eau souterraines en France</span>
+      </nav>
+    );
 }
 
 export default Navigation;
